55e815d9d9bed42e6f3939bc7bde6a1090742f20
Georgy Curnoff <geocurnoff@gmail.com>
2015-08-22 22:34:15 -0600

    target parsing added

--- pkg-invoke.sh
+++ pkg-invoke.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/bash
 
+extract_name() {
+	echo $1 | sed -ne 's@\(^[^[:space:]/]*\).*@\1@p'
+	# Sed demangling 
+	# s - replace command, 
+	# @ is delimiter 
+	# \( \) - grouping
+	# ^[^[:space:]/]* - match everything except whitespace and /
+	# \( \).* - match the rest of the string, whatever it is 
+	# @\1@ - replace matched string with the group \1
+	# p - print
+}
+
+extract_target() {
+	echo $1 | sed -ne 's@^[^[:space:]/]*/\([^[:space:]]*\)@\1@p'
+}
+
 # A command interface for packages
 
 [ "$#" -lt 2 ] && echo "USAGE: pkg-invoke.sh <command list> <package name>" 1>&2 && exit 1
@@ -10,7 +26,8 @@ PACKAGES_DIR=$(readlink -f `dirname $0`)
 
 . $PACKAGES_DIR/common.sh
 
-PKG_DIR_NAME="${@:$#}"
+LAST_ARGUMENT="${@:$#}"
+PKG_DIR_NAME=$(extract_name $LAST_ARGUMENT )
 
 dir_exists_in $PKG_DIR_NAME $PACKAGES_DIR || die "Package doesn't exist!"
 
@@ -28,12 +45,15 @@ else
     SRC=$PKGDIR/src
     SCRATCH=$PKGDIR/scratch
 	NAME=$(basename "$PACKAGES_DIR/$PKG_DIR_NAME/")
+	TARGET="$(extract_target $LAST_ARGUMENT)"
+	TARGET="${TARGET:-DEFAULT}"
 
 	RED='\033[1;31m'
 	NC='\033[0m' # No Color
-	printf "${RED}Invoking $1 command on package ${NAME-$2}${NC}\n"
+	printf "${RED}Invoking $1 command on package ${NAME-$2} for target ${TARGET} ${NC}\n"
 	[ -f "$1".cmd.sh ] && . "$1".cmd.sh || die "Command not found!"
 	popd > /dev/null
 fi
 
 exit 0
+
