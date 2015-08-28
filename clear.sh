482623fceadf44c6c28c6c4c5a20b8f7aae692cb
Georgy Curnoff <geocurnoff@gmail.com>
2015-08-27 23:59:34 -0600

    A script to clear all packages

---
+++ clear.sh
@@ -0,0 +1,11 @@
+
+LFS_SRC=$(readlink -f `dirname $0`)
+
+. $LFS_SRC/lib/die.sh
+
+PACKAGES_DIR=$LFS_SRC"/packages"
+
+for f in $PACKAGES_DIR/*; do
+	NAME=$(basename $f)
+	$LFS_SRC/pkg.sh clear $NAME
+done
