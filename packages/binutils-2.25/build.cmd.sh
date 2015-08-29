9e2ac30b0530a03a68e55b3a2f9c10eab8af311f
Georgy Curnoff <geocurnoff@gmail.com>
2015-08-29 12:04:27 -0600

    binutils-2.25 stage 2

--- packages/binutils-2.25/build.cmd.sh
+++ packages/binutils-2.25/build.cmd.sh
@@ -1,6 +1,14 @@
 
-if [ "$TARGET" = "initsys/1" ]; then
-    . initsys-build.sh
-else
-    . normal-build.sh
-fi
+case "$TARGET" in
+    initsys/1)
+		. initsys-1-build.sh
+        ;;
+    initsys/2)
+		. initsys-2-build.sh
+		;;
+    DEFAULT)
+        . normal-build.sh
+        ;;
+    *)
+        die "Unrecognized target $TARGET!"
+esac
---
+++ packages/binutils-2.25/initsys-2-build.sh
@@ -0,0 +1,36 @@
+
+# Don't remove this directory when rebuilding
+mkdir -v -p $SCRATCH || die
+
+cd $SCRATCH || die
+
+CC=$LFS_TGT-gcc                \
+AR=$LFS_TGT-ar                 \
+RANLIB=$LFS_TGT-ranlib         \
+$SRC/binutils-2.25/configure   \
+    --host=$LFS_TGT            \
+    --prefix=$TOOLS            \
+    --disable-nls              \
+    --disable-werror           \
+    --with-lib-path=$TOOLS/lib \
+    --with-sysroot
+
+# --host=$LFS_TGT  is this Okay?
+
+# Reset fake root directory
+rm -rf $BUILD &> /dev/null
+mkdir -v -p $BUILD
+
+# case $ARCHITECTURE in
+#   x86_64) mkdir -v $BUILD$TOOLS/lib && ln -sv lib $BUILD$TOOLS/lib64 ;;
+# esac
+
+make || die "Building $NAME failed."
+
+# Install to fake root
+make DESTDIR="$BUILD" install || die
+
+# Prepare for readjust
+make -C ld clean || die "ld clean failed"
+make -C ld LIB_PATH=/usr/lib:/lib || die "ld build failed"
+cp -v ld/ld-new $BUILD/$TOOLS/bin
