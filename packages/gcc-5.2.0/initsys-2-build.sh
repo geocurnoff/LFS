fea05bce7bce24800e231889ad081a243dd394e5
Georgy Curnoff <geocurnoff@gmail.com>
2015-09-11 00:53:35 -0600

    gcc/initsys/2 fixes

--- packages/gcc-5.2.0/initsys-2-build.sh
+++ packages/gcc-5.2.0/initsys-2-build.sh
@@ -7,22 +7,22 @@ mkdir -v -p $SCRATCH || die
 
 cd $SCRATCH > /dev/null || die
 
-CC=$LFS_TGT-gcc                                      \
-CXX=$LFS_TGT-g++                                     \
-AR=$LFS_TGT-ar                                       \
-RANLIB=$LFS_TGT-ranlib                               \
-$SRC/gcc-*/configure                                 \
-    --with-sysroot=$LFS                              \
-    --prefix=$TOOLS                                  \
-    --with-local-prefix=$TOOLS                       \
-    --with-native-system-header-dir=$TOOLS/include   \
-    --enable-languages=c,c++                         \
-    --disable-libstdcxx-pch                          \
-    --disable-multilib                               \
-    --disable-bootstrap                              \
-    --disable-libgomp || die "Configuring $NAME failed."
+# CC=$LFS_TGT-gcc                                      \
+# CXX=$LFS_TGT-g++                                     \
+# AR=$LFS_TGT-ar                                       \
+# RANLIB=$LFS_TGT-ranlib                               \
+# $SRC/gcc-*/configure                                 \
+#     --with-sysroot=$LFS                              \
+#     --prefix=$TOOLS                                  \
+#     --with-local-prefix=$TOOLS                       \
+#     --with-native-system-header-dir=$TOOLS/include   \
+#     --enable-languages=c,c++                         \
+#     --disable-libstdcxx-pch                          \
+#     --disable-multilib                               \
+#     --disable-bootstrap                              \
+#     --disable-libgomp || die "Configuring $NAME failed."
 
-make -j2 || die "Building $NAME failed."
+# make -j2 || die "Building $NAME failed."
 
 # Reset fake root directory
 rm -rf $BUILD &> /dev/null
@@ -32,8 +32,23 @@ mkdir -v -p $BUILD
 make DESTDIR=$BUILD install
 
 # Make limits file
-cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
-  $BUILD`dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed/limits.h
+LIMITS_DIR=$BUILD`dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/include-fixed
+mkdir -pv $LIMITS_DIR
+cat $SRC/*/gcc/limitx.h $SRC/*/gcc/glimits.h $SRC/*/gcc/limity.h > $LIMITS_DIR/limits.h
 
 # cc alias for gcc 
 ln -sv gcc $BUILD/tools/bin/cc
+
+# FIX: lib64 again...
+mv -v $BUILD$TOOLS/lib64/* $BUILD$TOOLS/lib
+rm -rv $BUILD$TOOLS/lib64/
+
+# Replace all "unknown" paths
+for f in $(find $BUILD -type f -name "*unknown*"); do
+	f_fixed=$(echo $f | sed "s@unknown@lfs@g")
+	mv -v $f $f_fixed
+done
+
+mv -v $BUILD/tools/include/c++/5.2.0/{x86_64-unknown-linux-gnu,x86_64-lfs-linux-gnu}
+mv -v $BUILD/tools/libexec/gcc/{x86_64-unknown-linux-gnu,x86_64-lfs-linux-gnu}
+cp -rv $BUILD/tools/lib/gcc/x86_64-unknown-linux-gnu/* $BUILD/tools/lib/gcc/x86_64-lfs-linux-gnu/
