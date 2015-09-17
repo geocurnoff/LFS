373919a93b2b4cce4f3e51d13b9bd431f2ab081c
Georgy Curnoff <geocurnoff@gmail.com>
2015-09-16 20:31:04 -0600

    diffutils-3.3 for initsys

---
+++ packages/diffutils-3.3/prepare.cmd.sh
@@ -0,0 +1,12 @@
+
+rm -rf $SRC &> /dev/null
+
+mkdir -p $SRC
+
+cd $SRC
+
+tar -xvf $CACHE/*.tar.* || die "Unpacking $NAME sources failed. Forgot to fetch?"
+
+cd ./*/
+
+sed -i 's:= @mkdir_p@:= /bin/mkdir -p:' po/Makefile.in.in
