11801fb140dac5c7c5f55be47882851211bb5f76
Georgy Curnoff <geocurnoff@gmail.com>
2015-09-28 16:45:28 -0600

    get-file

--- lib/get-file.sh
+++ lib/get-file.sh
@@ -3,11 +3,10 @@ get-file() {
     # $1 - urls
     # $2 - output file path
     # $3 - checksum
-    CHECKSUM=$(md5sum $2 | cut -f 1 -d " ")
-    [ $CHECKSUM = $3 ] && return 0
-    [ $3 = "1" ] && [ -f $2 ] && return 0
+    CHECKSUM=$(md5sum $2 2>/dev/null | cut -f 1 -d " ")
+    [ "$3" ] && [ "$CHECKSUM" = "$3" ] && printf_color green "Checksums match.\n" && return 0
     for u in $1; do
-          wget --timeout=10 --tries=3 $u -O $2 && return 0
+        wget --timeout=10 --tries=3 $u -O $2 && md5sum $2 2>/dev/null | cut -f 1 -d " " > $CACHE/$(basename $2)".md5" && success "Downloaded $1 to $2." && return 0
     done
     return 1
 }
