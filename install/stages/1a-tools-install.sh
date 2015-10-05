5fbd7bd234f37fa582499cf86008bfabd125cd1b
Geo Curnoff <geocurnoff@gmail.com>
2015-10-04 23:16:09 -0600

    better handling of /tool links

--- install/stages/1a-tools-install.sh
+++ install/stages/1a-tools-install.sh
@@ -4,14 +4,23 @@
 LFS_SRC=$(readlink -f `dirname $0`)/../..
 . $LFS_SRC/lib/die.sh
 . $LFS_SRC/packages/shared/tools.cfg.sh
+. $LFS_SRC/lib/prompting.sh
 
-if [ -e /$TOOLS_PREFIX ]; then
-	die "File already exists: \"/$TOOLS_PREFIX\"!"
+if [ -e "/$TOOLS_PREFIX" ]; then
+  # Check if it's a link
+  [ -h "/$TOOLS_PREFIX" ] || die "/$TOOLS_PREFIX exists but it's not a link!"
+  
+  # Check if it points to the correct location
+  [ "$LFS/$TOOLS_PREFIX" == $(readlink -f "/$TOOLS_PREFIX") ] \
+  && echo "Reusing existing link to tools..." \
+  || die "/$TOOLS_PREFIX already exists, but it's pointing to wrong location!"
+else
+  ln -s $LFS/$TOOLS_PREFIX /$TOOLS_PREFIX || die
 fi
 
-mkdir -pv $LFS/$TOOLS_PREFIX && \
-sudo ln -s $LFS/$TOOLS_PREFIX /$TOOLS_PREFIX || die
-
+# Create LFS and tools directories
+mkdir -pv $LFS/$TOOLS_PREFIX || die
+ 
 env -i \
 HOME=$HOME \
 TERM=$TERM PS1='\u:\w\$ ' \
